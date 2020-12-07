0a13261c4b38fe7f9c8122a230fbd665ce5260cf
Héctor Molinero Fernández <hector@molinero.dev>
2020-12-07 15:02:30 +0100

    Replaced "echo" with "printf"

--- scripts/bin/qemu-binfmt-register.sh
+++ scripts/bin/qemu-binfmt-register.sh
@@ -8,8 +8,8 @@ set -eu
 QEMU_BIN_DIR=${QEMU_BIN_DIR:-/usr/bin}
 
 if [ ! -d /proc/sys/fs/binfmt_misc ]; then
-	echo 'No binfmt support in the kernel.'
-	echo '  Try: "/sbin/modprobe binfmt_misc" from the host'
+	printf '%s\n' 'No binfmt support in the kernel.'
+	printf '%s\n' '  Try: "/sbin/modprobe binfmt_misc" from the host'
 	exit 1
 fi
 
@@ -19,7 +19,7 @@ fi
 
 if [ "${1-}" = '--reset' ]; then
 	shift
-	find /proc/sys/fs/binfmt_misc -type f -name 'qemu-*' -exec sh -c 'echo -1 > "$1"' _ '{}' ';'
+	find /proc/sys/fs/binfmt_misc -type f -name 'qemu-*' -exec sh -c 'printf -- "-1" > "$1"' _ '{}' ';'
 fi
 
 exec "${QEMU_BIN_DIR:?}"/qemu-binfmt-conf.sh --qemu-path "${QEMU_BIN_DIR:?}" --qemu-suffix '-static' "$@"
